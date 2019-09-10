1773ea8925d5d698aad339752f9c04646f808634
Nathaniel Mosco <natemosco@gmail.com>
2019-09-10 17:52:03 -0400

    created the NumberButton Function in NumberButton.js Ex/im ported into Numbers.js

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import data from "../../../data"; // object includes {numbers, operators, specials}
-
+import {NumberButton} from "./NumberButton";
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
 /* 
@@ -16,6 +16,9 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {data.numbers.map((currVal, index) =>{
+        <NumberButton key={index} number={currVal} />
+      })}
     </div>
   );
 };
