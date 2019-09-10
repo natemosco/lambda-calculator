1c66dec06b4e3dc317c02b5fdd9976e79a49d0bb
Nathaniel Mosco <natemosco@gmail.com>
2019-09-10 18:13:56 -0400

    SpecialButton function created ex/im ported to Specials; changed naming convention special and operator;

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -14,7 +14,7 @@ const Operators = () => {
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
        {data.operators.map( (e,i)=> 
-       <OperatorButton key={index} operator={e}/>
+       <OperatorButton key={index} operatorSymbol={e}/>
        )}
        
     </div>
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -12,6 +12,9 @@ const Specials = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {data.specials.map((e,i)=>{
+        <SpecialButton key={i} specialSymbol={e}/>
+      })} 
     </div>
   );
 };
