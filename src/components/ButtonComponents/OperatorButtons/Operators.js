9a909350b13ee53c2c86f6307b9ff06b158e2b8b
Nathaniel Mosco <natemosco@gmail.com>
2019-09-10 18:01:13 -0400

    created OperatorButton function ex/im port to Operator.js and map through data

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,5 +1,6 @@
 import React from "react";
 import data from "../../../data"; // object includes {numbers, operators, specials}
+import {OperatorButton} from "./OperatorButton"
 //import any components needed
 
 //Import your array data to from the provided data file
@@ -12,6 +13,10 @@ const Operators = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {data.operators.map( (e,i)=> 
+       <OperatorButton key={index} operator={e}/>
+       )}
+       
     </div>
   );
 };
