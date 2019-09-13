1a15283eae906018a274ba27e6c2794a2b0fffcb
Nathaniel Mosco <natemosco@gmail.com>
2019-09-12 21:25:56 -0400

    yarn build => build folder changed name to docs

---
+++ docs/precache-manifest.88ac4c7742ef660500f94dfc08b93ddd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1602c0a6baf29dc094ce93d9d05e81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae6462bfadf4c7c52431",
+    "url": "/static/css/main.96ee364c.chunk.css"
+  },
+  {
+    "revision": "7684408bd5554961719d",
+    "url": "/static/js/2.4f057907.chunk.js"
+  },
+  {
+    "revision": "ae6462bfadf4c7c52431",
+    "url": "/static/js/main.087d2b64.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
